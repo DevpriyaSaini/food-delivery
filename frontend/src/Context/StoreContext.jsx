e7bb113984672bfb376b2deeb4b589e00343208c
DevpriyaSaini <devpriyasaini6@gmail.com>
2025-05-28 10:57:09 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const StoreContext = createContext(null);
 
-const url="http://localhost:4000";
+const url="https://food-delivery-backend-sydz.onrender.com";
 
 const StoreContextProvider = (props) => {
 
