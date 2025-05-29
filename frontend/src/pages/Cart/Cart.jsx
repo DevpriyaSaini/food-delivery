5180aa5093c8166f9e9c65f2dd08398432435685
DevpriyaSaini <devpriyasaini6@gmail.com>
2025-05-29 22:36:17 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -6,59 +6,61 @@ const url="https://food-delivery-backend-sydz.onrender.com";
 function Cart() {
   const{cartItems,food_list,removeCart,gettotal}=useContext(StoreContext)
   return (
-
-    <div className='cart'>
-      <div className="cart-item">
-        <div className="cart-item-title">
-          <p>Items</p>
-          <p>Title</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p> Remove</p>
-        </div>
-        <br />
-        <hr />
-        {food_list.map((item,index)=>{
-         if(cartItems[item._id]>0) {
+  <div className='cart'>
+    <div className="cart-item">
+      <div className="cart-item-title">
+        <p>Items</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <br />
+      <hr />
+      {food_list.map((item) => {
+        if (cartItems[item._id] > 0) {
           return (
-            <div className="cart-item-title card-items-item">
-              <img src={url+"/Image/"+item.image} alt="" />
+            <div key={item._id} className="cart-item-title card-items-item">
+              <img src={`${url}/Image/${item.image}`} alt={item.name} />
               <p>{item.name}</p>
               <p>${item.Price}</p>
               <p>{cartItems[item._id]}</p>
-              <p>${item.Price*cartItems[item._id]}</p>
-              <p onClick={()=>removeCart(item._id)}>x</p>
+              <p>${item.Price * cartItems[item._id]}</p>
+              <p onClick={() => removeCart(item._id)} style={{ cursor: 'pointer' }}>x</p>
             </div>
-          )
-         }
-        })}
-      </div>
+          );
+        }
+        return null;
+      })}
+    </div>
 
-      <div className="maindiv">
+    <div className="maindiv">
       <div className="cart-bottom">
         <div className="cart-total">
           <h2>Cart Totals</h2>
         </div>
         <div className="cart-total-detail">
           <p>Subtotal</p>
-          <p>{gettotal()}</p>
+          <p>${gettotal()}</p>
           <hr />
           <div className="cart-total-detail">
             <p>Delivery Fee</p>
-            <p>${gettotal()===0?0:2}</p>
+            <p>${gettotal() === 0 ? 0 : 2}</p>
           </div>
-          <hr/>
+          <hr />
           <div className="cart-total-details">
             <b>Total</b>
-            <b>${gettotal()===0?0:gettotal()+2}</b>
+            <b>${gettotal() === 0 ? 0 : gettotal() + 2}</b>
           </div>
         </div>
-        <Link to={'/order'}><button>Proceed to checkout</button></Link>
-       </div>
+        <Link to='/order'>
+          <button>PROCEED TO CHECKOUT</button>
+        </Link>
+      </div>
       <div className="cart-promocode">
         <div>
-          <p>If you have a promo code,Enter it here</p>
+          <p>If you have a promo code, enter it here</p>
           <div className="card-promocode-input">
             <input type="text" placeholder='Promo Code' />
             <button>Submit</button>
@@ -66,8 +68,8 @@ function Cart() {
         </div>
       </div>
     </div>
-   </div>
-  )
+  </div>
+);
 }
 
 export default Cart
